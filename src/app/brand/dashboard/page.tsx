2b4738eaf9f49b0fe64613360c0f670c26838246
Adrita Guha <adrita.g.2005@gmail.com>
2026-08-16 00:02:06 +0530

    proposal redirection

--- src/app/brand/dashboard/page.tsx
+++ src/app/brand/dashboard/page.tsx
@@ -180,7 +180,7 @@ export default function BrandDashboardWelcomePage() {
 									return (
 										<Link
 											key={prop.id}
-											href={`/brand/dashboard/proposals?proposalId=${prop.id}`}
+											href={`/brand/dashboard/proposal/${prop.id}`}
 											className="group relative cursor-pointer bg-white border-[3px] border-black rounded-[20px] shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] hover:shadow-[1px_1px_0px_0px_rgba(0,0,0,1)] hover:translate-x-[2px] hover:translate-y-[2px] transition-all overflow-hidden flex flex-row w-[380px] shrink-0"
 										>
 											{/* Image / Logo */}
